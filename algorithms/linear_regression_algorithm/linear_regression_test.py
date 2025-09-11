ab3b4522b0fc9e2f47ba9e6dfc8420dbdbafe19d
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2025-09-11 19:53:09 +0300

    create :linear_regression_folder

--- algorithms/linear_regression_algorithm/linear_regression_test.py
+++ algorithms/linear_regression_algorithm/linear_regression_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 X,Y=datasets.make_regression(n_samples=100,n_features=1,noise=20,random_state=4)
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1234)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=4)
 
 from linear_regression import LinearRegression
 regressor=LinearRegression(lr=0.1,n_iterations=1000)
