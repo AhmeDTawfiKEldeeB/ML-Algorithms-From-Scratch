03f62239593e0847f6d5c3eedebf216fef47b2d2
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2025-09-12 15:12:52 +0300

    finish algorithm: logistic_regression

---
+++ algorithms/logistic_regression_algorithm/logistic_regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class LogisticRegression:
+
+    def __init__(self,lr,n_iters):
+        self.lr=lr
+        self.n_iters=n_iters
+        self.weights=None
+        self.bias=None
+
+    def fit(self,X,Y):
+        #initialize parameters
+        n_samples,n_features=X.shape
+        self.weights=np.zeros(n_features)
+        self.bias=0
+        #gradient descent
+        for i in range(self.n_iters):
+            linear_model=np.dot(X,self.weights)+self.bias
+            y_predict=self._segmoid(linear_model)
+            dw=(1/n_samples)*np.dot(X.T,(y_predict-Y))
+            db=(1/n_samples)*np.sum(y_predict-Y)
+            self.weights-=self.lr*dw
+            self.bias-=self.lr*db
+
+    def predict(self,X):
+        linear_model=np.dot(X,self.weights)+self.bias
+        y_predict=self._segmoid(linear_model)
+        y_predict_class=[1 if i>0.5 else 0 for i in y_predict]
+        return y_predict_class
+    def _segmoid(self,s):
+        return (1/(1+np.exp(s)))
---
+++ algorithms/logistic_regression_algorithm/logistic_regression_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+data_set=datasets.load_breast_cancer()
+X,Y=data_set.data,data_set.target
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1234)
+
+from logistic_regression import LogisticRegression
+logistic_reg=LogisticRegression(lr=0.001,n_iters=1000)
+logistic_reg.fit(X_train,Y_train)
+predictions=logistic_reg.predict(X_test)
+
+def accuracy(y_true,y_pred):
+    accuracy=np.sum(y_true==y_pred)/len(y_true)
+    return accuracy
+print ('The accuracy of the model is:',accuracy(Y_test,predictions))   
+    
